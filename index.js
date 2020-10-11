d0c72ba326d92ab97d1a728395af90b5ca39587b
EnzoKilm <enzo-beauchamp@live.fr>
2020-10-11 15:46:01 +0200

    Incoming bot actus command

--- index.js
+++ index.js
@@ -22,7 +22,28 @@ client.on("message", function(message) {
     if (command === "ping") {
         const timeTaken = Date.now() - message.createdTimestamp;
         message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
-    } 
+    }
+
+    if (command === "actus") {
+        const author = message.author;
+        const embed = new Discord.MessageEmbed()
+            .setColor('#ffffff')
+            .setTitle('Un nouveau bot arrive sur le discord !')
+            .setURL('https://i.giphy.com/media/xThuWhoaNyNBjTGERa/giphy.webp')
+            .setAuthor('Campus Academy', 'https://i.imgur.com/JvgbYON.png', 'https://campus.academy/')
+            .setThumbnail('https://i.imgur.com/pShRI7I.png')
+            .addFields(
+                { name: 'Plein de fonctionnalités sont à venir...', value: 'N\'hésite pas à soumettre tes idées dans le channel <#764834554783334460> !' },
+            )
+            .setImage('https://media1.tenor.com/images/915bd1719d2dee1adfc04d1a87a83810/tenor.gif')
+            .setTimestamp()
+            .setFooter(`Requested by @${author.tag}`, `${author.avatarURL()}`);
+        
+        // Sending the embed to the channel where the message was posted
+        message.channel.send(embed);
+        // Deleting the message
+        message.delete();
+    }
 });
 
 client.login(config.BOT_TOKEN);
